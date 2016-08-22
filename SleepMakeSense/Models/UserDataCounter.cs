2265acf38b734eaeb190ec33a9bbe5c61e331baf
Sean <spn4152@gmail.com>
2016-08-22 22:06:37 +1000

    Cleaning some code up. Working on making sence on how to  improve the data mining with OOP techniques.

---
+++ SleepMakeSense/Models/UserDataCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SleepMakeSense.Models
+{
+    public class UserDataCounter
+    {
+        public int CNTcaloriesIn { get; set; }
+        public int CNTwater { get; set; }
+        public int CNTcaloriesOut { get; set; }
+        public int CNTsteps { get; set; }
+        public int CNTweight { get; set; }
+        public int CNTfat { get; set; }
+        public int CNTwakeUpFreshness { get; set; }
+        public int CNTcoffee { get; set; }
+        public int CNTcoffeeTime { get; set; }
+        public int CNTalcohol { get; set; }
+        public int CNTmood { get; set; }
+        public int CNTstress { get; set; }
+        public int CNTtiredness { get; set; }
+        public int CNTdream { get; set; }
+        public int CNTdigitalDev { get; set; },
+        public int CNTlight { get; set; }
+        public int CNTnapDuration { get; set; }
+        public int CNTnapTime { get; set; }
+        public int CNTsocialActivity { get; set; }
+        public int CNTdinnerTime { get; set; }
+        public int CNTexerciseTime { get; set; }
+        public int CNTambientTemp { get; set; }
+        public int CNTambientHumd { get; set; }
+        public int CNTbodyTemp { get; set; }
+        public int CNThormone { get; set; }
+
+    }
+}
---
+++ SleepMakeSense/Models/UserDataDouble.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SleepMakeSense.Models
+{
+    public class UserDataDouble
+    {
+        public int Id { get; set; }
+
+        public double Steps { get; set; }
+
+        public double MinutesAsleep { get; set; }
+
+        public DateTime DateStamp { get; set; }
+
+        public double Water { get; set; }
+
+        public double Distance { get; set; }
+
+        public double MinutesSedentary { get; set; }
+
+        public double MinutesVeryActive { get; set; }
+
+        public double AwakeningsCount { get; set; }
+
+        public double TimeEnteredBed { get; set; }
+
+        public double Weight { get; set; }
+
+        public double MinutesAwake { get; set; }
+
+        public double TimeInBed { get; set; }
+
+        public double MinutesToFallAsleep { get; set; }
+
+        public double MinutesAfterWakeup { get; set; }
+
+        public double CaloriesIn { get; set; }
+
+        public double CaloriesOut { get; set; }
+
+        public double MinutesLightlyActive { get; set; }
+
+        public double MinutesFairlyActive { get; set; }
+
+        public double ActivityCalories { get; set; }
+
+        public double BMI { get; set; }
+
+        public double Fat { get; set; }
+
+        public double SleepEfficiency { get; set; }
+
+        public double WakeUpFreshness { get; set; }
+
+        public double Coffee { get; set; }
+
+        public double CoffeeTime { get; set; }
+
+        public double Alcohol { get; set; }
+
+        public double Mood { get; set; }
+
+        public double Stress { get; set; }
+
+        public double Tiredness { get; set; }
+
+        public double Dream { get; set; }
+
+        public double DigitalDev { get; set; }
+
+        public double Light { get; set; }
+
+        public double NapDuration { get; set; }
+
+        public double NapTime { get; set; }
+
+        public double SocialActivity { get; set; }
+
+        public double DinnerTime { get; set; }
+
+        public double AmbientTemp { get; set; }
+
+        public double AmbientHumd { get; set; }
+
+        public double ExerciseTime { get; set; }
+
+        public double BodyTemp { get; set; }
+
+        public double Hormone { get; set; }
+
+    }
+}

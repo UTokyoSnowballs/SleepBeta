9fa4cacc16b708bcea7013da435fda07c83c7625
TomJC <thomas_185@hotmail.com>
2016-08-23 14:15:24 +1000

    Added icons for nav and more fontawesome files

--- SleepMakeSense/App_Start/BundleConfig.cs
+++ SleepMakeSense/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace SleepMakeSense
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
+
+            ;
         }
     }
 }

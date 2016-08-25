b661f35212c63c75e6ba8052501127cd68839603
Sean Nolan <s.nolan@outlook.com.au>
2016-08-25 20:51:39 +1000

    Deploy Fix

--- SleepMakeSense/App_Start/BundleConfig.cs
+++ SleepMakeSense/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SleepMakeSense
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Layout.css",
                       "~/Content/Site.css"));
 
             ;

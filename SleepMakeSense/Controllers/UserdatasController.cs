3451b63767ac2ab5854f90e1321c1253d07185a2
Sean Nolan <s.nolan@outlook.com.au>
2016-09-12 11:50:29 +1000

    Revert "UserQuestions added to the model generation."
    This reverts commit 172118db911476d246ba3ca1d9ae650664a0cd62.

--- SleepMakeSense/Controllers/UserdatasController.cs
+++ SleepMakeSense/Controllers/UserdatasController.cs
@@ -596,7 +596,7 @@ namespace SleepMakeSense.Controllers
             //Need to define it here. Cannot use the one defined in FitbitController.cs.
 
             Models.Database Db = new Models.Database();
-            var model = new MyViewModel();
+
             DateTime date = DateTime.UtcNow.AddDays(-40);
 
             List<Userdata> results = (from a in Db.Userdatas
@@ -604,27 +604,9 @@ namespace SleepMakeSense.Controllers
                                      orderby  a.DateStamp
                                             select a).ToList();
 
-            var dataQuery = from a in Db.UserQuestions
-                            where a.AspNetUserId.Equals(userId)
-                            select a;
 
-            foreach (UserQuestion data in dataQuery)
-            {
-                if (data.AspNetUserId == userId)
-                {
-                    ViewBag.Message = "Enter Your Daily Habits";
-
-                    model.UserQuestion = data;
-
-                }
-            }
-
-
-
-
-
-            //   List<DiaryData> diaryData = new List<DiaryData>();
-
+         //   List<DiaryData> diaryData = new List<DiaryData>();
+        
 
             int CNTcaloriesIn = 0, CNTwater = 0, CNTcaloriesOut = 0, CNTsteps = 0, CNTweight = 0, CNTfat = 0;
             int CNTwakeUpFreshness = 0, CNTcoffee = 0, CNTcoffeeTime = 0, CNTalcohol = 0, CNTmood = 0, CNTstress = 0,
@@ -3813,7 +3795,7 @@ namespace SleepMakeSense.Controllers
 
             }
 
-
+            var model = new MyViewModel();
             model.AllData = results;
             model.CorrCoefficient = CoefficientList;
 

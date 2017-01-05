ad3d05dec3b4ef0da9ed71b0e6e04e88d84fd88d
Sean <spn4152@gmail.com>
2017-01-06 00:09:11 +1000

    Some updates to the Fitbit.net portable Library. Need to test when token expires

--- SleepMakeSense/OAuth2/DefaultTokenManager.cs
+++ SleepMakeSense/OAuth2/DefaultTokenManager.cs
@@ -1,33 +1,41 @@
 ﻿namespace SleepMakeSense.Controllers
 {
-    using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Threading.Tasks;
 
-    internal class DefaultTokenManager : ITokenManager
-    {
-        private static string FitbitOauthPostUrl => "https://api.fitbit.com/oauth2/token";
+        using System.Collections.Generic;
+        using System.Net.Http;
+        using System.Threading.Tasks;
 
-        public async Task<OAuth2AccessToken> RefreshTokenAsync(FitbitClient client)
+        internal class DefaultTokenManager : ITokenManager
         {
-            string postUrl = FitbitOauthPostUrl;
+            private static string FitbitOauthPostUrl => "https://api.fitbit.com/oauth2/token";
 
-            var content = new FormUrlEncodedContent(new[]
+            public async Task<OAuth2AccessToken> RefreshTokenAsync(FitbitClient client)
             {
+                string postUrl = FitbitOauthPostUrl;
+
+                var content = new FormUrlEncodedContent(new[]
+                {
                 new KeyValuePair<string, string>("grant_type", "refresh_token"),
                 new KeyValuePair<string, string>("refresh_token", client.AccessToken.RefreshToken),
             });
 
+                HttpClient httpClient;
+                if (client.HttpClient == null)
+                {
+                    httpClient = new HttpClient();
+                }
+                else
+                {
+                    httpClient = client.HttpClient;
+                }
 
-            var httpClient = new HttpClient();
+                var clientIdConcatSecret = OAuth2Helper.Base64Encode(client.AppCredentials.ClientId + ":" + client.AppCredentials.ClientSecret);
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", clientIdConcatSecret);
 
-            var clientIdConcatSecret = OAuth2Helper.Base64Encode(client.AppCredentials.ClientId + ":" + client.AppCredentials.ClientSecret);
-            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", clientIdConcatSecret);
+                HttpResponseMessage response = await httpClient.PostAsync(postUrl, content);
+                string responseString = await response.Content.ReadAsStringAsync();
 
-            HttpResponseMessage response = await httpClient.PostAsync(postUrl, content);
-            string responseString = await response.Content.ReadAsStringAsync();
-
-            return OAuth2Helper.ParseAccessTokenResponse(responseString);
+                return OAuth2Helper.ParseAccessTokenResponse(responseString);
+            }
         }
     }
-}

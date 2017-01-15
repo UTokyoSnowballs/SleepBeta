d7fed251d9eb467c4ddd978047aa7ecb06327682
Lucy Liang <panda198893@hotmail.com>
2017-01-15 09:42:06 +0900

    commit some random changes in order to rollback

---
+++ SleepMakeSense/Migrations/201701141340231_test6.cs
@@ -0,0 +1,16 @@
+namespace SleepMakeSense.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test6 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
